b5137c9eb0156304adb0165513aa2e62b0f056e7
JaminYC <a20212422@pucp.edu.pe>
2023-11-07 13:44:52 -0500

    Uso de SQL CRUD

--- WeatherStationView/SensorMaintenanceForm.h
+++ WeatherStationView/SensorMaintenanceForm.h
@@ -90,14 +90,34 @@ namespace WeatherStationView {
 	private: System::Windows::Forms::TextBox^ UnidadTBox;
 	private: System::Windows::Forms::Label^ UnidadTLabel;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column6;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ UnidadTempdgv;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column4;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Temperatura;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ UnidadTemperatura;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Humedad;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ NivelCO;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ CalidadAire;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column5;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Fecha;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dgvHora;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -148,11 +168,11 @@ namespace WeatherStationView {
 			this->UnidadTBox = (gcnew System::Windows::Forms::TextBox());
 			this->UnidadTLabel = (gcnew System::Windows::Forms::Label());
 			this->Column6 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->UnidadTempdgv = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Temperatura = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->UnidadTemperatura = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Humedad = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->NivelCO = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->CalidadAire = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column5 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Fecha = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->dgvHora = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
@@ -162,72 +182,81 @@ namespace WeatherStationView {
 			// TempLabel
 			// 
 			this->TempLabel->AutoSize = true;
-			this->TempLabel->Location = System::Drawing::Point(33, 25);
+			this->TempLabel->Location = System::Drawing::Point(44, 31);
+			this->TempLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->TempLabel->Name = L"TempLabel";
-			this->TempLabel->Size = System::Drawing::Size(67, 13);
+			this->TempLabel->Size = System::Drawing::Size(85, 16);
 			this->TempLabel->TabIndex = 0;
 			this->TempLabel->Text = L"Temperatura";
 			// 
 			// HumLabel
 			// 
 			this->HumLabel->AutoSize = true;
-			this->HumLabel->Location = System::Drawing::Point(33, 49);
+			this->HumLabel->Location = System::Drawing::Point(44, 60);
+			this->HumLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->HumLabel->Name = L"HumLabel";
-			this->HumLabel->Size = System::Drawing::Size(53, 13);
+			this->HumLabel->Size = System::Drawing::Size(67, 16);
 			this->HumLabel->TabIndex = 1;
 			this->HumLabel->Text = L"Humedad";
 			// 
 			// COLabel
 			// 
 			this->COLabel->AutoSize = true;
-			this->COLabel->Location = System::Drawing::Point(33, 75);
+			this->COLabel->Location = System::Drawing::Point(44, 92);
+			this->COLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->COLabel->Name = L"COLabel";
-			this->COLabel->Size = System::Drawing::Size(94, 13);
+			this->COLabel->Size = System::Drawing::Size(115, 16);
 			this->COLabel->TabIndex = 2;
 			this->COLabel->Text = L"Concentración CO";
 			// 
 			// TempBox
 			// 
-			this->TempBox->Location = System::Drawing::Point(155, 22);
+			this->TempBox->Location = System::Drawing::Point(207, 27);
+			this->TempBox->Margin = System::Windows::Forms::Padding(4);
 			this->TempBox->Name = L"TempBox";
-			this->TempBox->Size = System::Drawing::Size(100, 20);
+			this->TempBox->Size = System::Drawing::Size(132, 22);
 			this->TempBox->TabIndex = 3;
 			// 
 			// HumBox
 			// 
-			this->HumBox->Location = System::Drawing::Point(155, 49);
+			this->HumBox->Location = System::Drawing::Point(207, 60);
+			this->HumBox->Margin = System::Windows::Forms::Padding(4);
 			this->HumBox->Name = L"HumBox";
-			this->HumBox->Size = System::Drawing::Size(100, 20);
+			this->HumBox->Size = System::Drawing::Size(132, 22);
 			this->HumBox->TabIndex = 4;
 			// 
 			// COBox
 			// 
-			this->COBox->Location = System::Drawing::Point(155, 75);
+			this->COBox->Location = System::Drawing::Point(207, 92);
+			this->COBox->Margin = System::Windows::Forms::Padding(4);
 			this->COBox->Name = L"COBox";
-			this->COBox->Size = System::Drawing::Size(100, 20);
+			this->COBox->Size = System::Drawing::Size(132, 22);
 			this->COBox->TabIndex = 5;
 			// 
 			// AirQBox
 			// 
-			this->AirQBox->Location = System::Drawing::Point(431, 49);
+			this->AirQBox->Location = System::Drawing::Point(575, 60);
+			this->AirQBox->Margin = System::Windows::Forms::Padding(4);
 			this->AirQBox->Name = L"AirQBox";
-			this->AirQBox->Size = System::Drawing::Size(100, 20);
+			this->AirQBox->Size = System::Drawing::Size(132, 22);
 			this->AirQBox->TabIndex = 6;
 			// 
 			// AirQLabel
 			// 
 			this->AirQLabel->AutoSize = true;
-			this->AirQLabel->Location = System::Drawing::Point(309, 52);
+			this->AirQLabel->Location = System::Drawing::Point(412, 64);
+			this->AirQLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->AirQLabel->Name = L"AirQLabel";
-			this->AirQLabel->Size = System::Drawing::Size(63, 13);
+			this->AirQLabel->Size = System::Drawing::Size(81, 16);
 			this->AirQLabel->TabIndex = 7;
 			this->AirQLabel->Text = L"Calidad Aire";
 			// 
 			// AgregarBtn
 			// 
-			this->AgregarBtn->Location = System::Drawing::Point(36, 134);
+			this->AgregarBtn->Location = System::Drawing::Point(48, 165);
+			this->AgregarBtn->Margin = System::Windows::Forms::Padding(4);
 			this->AgregarBtn->Name = L"AgregarBtn";
-			this->AgregarBtn->Size = System::Drawing::Size(75, 23);
+			this->AgregarBtn->Size = System::Drawing::Size(100, 28);
 			this->AgregarBtn->TabIndex = 10;
 			this->AgregarBtn->Text = L"Agregar";
 			this->AgregarBtn->UseVisualStyleBackColor = true;
@@ -235,9 +264,10 @@ namespace WeatherStationView {
 			// 
 			// ModificarBtn
 			// 
-			this->ModificarBtn->Location = System::Drawing::Point(228, 134);
+			this->ModificarBtn->Location = System::Drawing::Point(304, 165);
+			this->ModificarBtn->Margin = System::Windows::Forms::Padding(4);
 			this->ModificarBtn->Name = L"ModificarBtn";
-			this->ModificarBtn->Size = System::Drawing::Size(75, 23);
+			this->ModificarBtn->Size = System::Drawing::Size(100, 28);
 			this->ModificarBtn->TabIndex = 11;
 			this->ModificarBtn->Text = L"Modificar";
 			this->ModificarBtn->UseVisualStyleBackColor = true;
@@ -245,9 +275,10 @@ namespace WeatherStationView {
 			// 
 			// EliminarBtn
 			// 
-			this->EliminarBtn->Location = System::Drawing::Point(447, 134);
+			this->EliminarBtn->Location = System::Drawing::Point(596, 165);
+			this->EliminarBtn->Margin = System::Windows::Forms::Padding(4);
 			this->EliminarBtn->Name = L"EliminarBtn";
-			this->EliminarBtn->Size = System::Drawing::Size(75, 23);
+			this->EliminarBtn->Size = System::Drawing::Size(100, 28);
 			this->EliminarBtn->TabIndex = 12;
 			this->EliminarBtn->Text = L"Eliminar";
 			this->EliminarBtn->UseVisualStyleBackColor = true;
@@ -258,20 +289,22 @@ namespace WeatherStationView {
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(9) {
 				this->Column6,
-					this->Column1, this->UnidadTempdgv, this->Column2, this->Column3, this->Column4, this->Column5, this->Fecha, this->dgvHora
+					this->Temperatura, this->UnidadTemperatura, this->Humedad, this->NivelCO, this->CalidadAire, this->Column5, this->Fecha, this->dgvHora
 			});
-			this->dataGridView1->Location = System::Drawing::Point(12, 179);
+			this->dataGridView1->Location = System::Drawing::Point(16, 220);
+			this->dataGridView1->Margin = System::Windows::Forms::Padding(4);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->Size = System::Drawing::Size(741, 177);
+			this->dataGridView1->Size = System::Drawing::Size(988, 218);
 			this->dataGridView1->TabIndex = 13;
 			this->dataGridView1->CellClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &SensorMaintenanceForm::TableCellClick);
 			// 
 			// CerrarBtn
 			// 
-			this->CerrarBtn->Location = System::Drawing::Point(650, 134);
+			this->CerrarBtn->Location = System::Drawing::Point(867, 165);
+			this->CerrarBtn->Margin = System::Windows::Forms::Padding(4);
 			this->CerrarBtn->Name = L"CerrarBtn";
-			this->CerrarBtn->Size = System::Drawing::Size(75, 23);
+			this->CerrarBtn->Size = System::Drawing::Size(100, 28);
 			this->CerrarBtn->TabIndex = 14;
 			this->CerrarBtn->Text = L"Cerrar";
 			this->CerrarBtn->UseVisualStyleBackColor = true;
@@ -280,9 +313,10 @@ namespace WeatherStationView {
 			// UbiGeoLabel
 			// 
 			this->UbiGeoLabel->AutoSize = true;
-			this->UbiGeoLabel->Location = System::Drawing::Point(309, 82);
+			this->UbiGeoLabel->Location = System::Drawing::Point(412, 101);
+			this->UbiGeoLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->UbiGeoLabel->Name = L"UbiGeoLabel";
-			this->UbiGeoLabel->Size = System::Drawing::Size(110, 13);
+			this->UbiGeoLabel->Size = System::Drawing::Size(138, 16);
 			this->UbiGeoLabel->TabIndex = 16;
 			this->UbiGeoLabel->Text = L"Ubicacion Geografica";
 			// 
@@ -290,26 +324,26 @@ namespace WeatherStationView {
 			// 
 			this->UbigeoCombo->FormattingEnabled = true;
 			this->UbigeoCombo->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"FACI", L"CIA", L"BIBLIOTECA CENTRAL", L"TINKUY" });
-			this->UbigeoCombo->Location = System::Drawing::Point(431, 78);
+			this->UbigeoCombo->Location = System::Drawing::Point(575, 96);
+			this->UbigeoCombo->Margin = System::Windows::Forms::Padding(4);
 			this->UbigeoCombo->Name = L"UbigeoCombo";
-			this->UbigeoCombo->Size = System::Drawing::Size(121, 21);
+			this->UbigeoCombo->Size = System::Drawing::Size(160, 24);
 			this->UbigeoCombo->TabIndex = 17;
 			// 
 			// UnidadTBox
 			// 
-			this->UnidadTBox->Location = System::Drawing::Point(433, 22);
-			this->UnidadTBox->Margin = System::Windows::Forms::Padding(2);
+			this->UnidadTBox->Location = System::Drawing::Point(577, 27);
+			this->UnidadTBox->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->UnidadTBox->Name = L"UnidadTBox";
-			this->UnidadTBox->Size = System::Drawing::Size(76, 20);
+			this->UnidadTBox->Size = System::Drawing::Size(100, 22);
 			this->UnidadTBox->TabIndex = 19;
 			// 
 			// UnidadTLabel
 			// 
 			this->UnidadTLabel->AutoSize = true;
-			this->UnidadTLabel->Location = System::Drawing::Point(309, 25);
-			this->UnidadTLabel->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->UnidadTLabel->Location = System::Drawing::Point(412, 31);
 			this->UnidadTLabel->Name = L"UnidadTLabel";
-			this->UnidadTLabel->Size = System::Drawing::Size(104, 13);
+			this->UnidadTLabel->Size = System::Drawing::Size(132, 16);
 			this->UnidadTLabel->TabIndex = 18;
 			this->UnidadTLabel->Text = L"Unidad Temperatura";
 			// 
@@ -320,38 +354,40 @@ namespace WeatherStationView {
 			this->Column6->Name = L"Column6";
 			this->Column6->Width = 125;
 			// 
-			// Column1
+			// Temperatura
 			// 
-			this->Column1->HeaderText = L"Temperatura";
-			this->Column1->MinimumWidth = 6;
-			this->Column1->Name = L"Column1";
-			this->Column1->Width = 125;
+			this->Temperatura->HeaderText = L"Temperatura";
+			this->Temperatura->MinimumWidth = 6;
+			this->Temperatura->Name = L"Temperatura";
+			this->Temperatura->Width = 125;
 			// 
-			// UnidadTempdgv
+			// UnidadTemperatura
 			// 
-			this->UnidadTempdgv->HeaderText = L"Unidad Temperatura";
-			this->UnidadTempdgv->Name = L"UnidadTempdgv";
+			this->UnidadTemperatura->HeaderText = L"UnidadTemperatura";
+			this->UnidadTemperatura->MinimumWidth = 6;
+			this->UnidadTemperatura->Name = L"UnidadTemperatura";
+			this->UnidadTemperatura->Width = 125;
 			// 
-			// Column2
+			// Humedad
 			// 
-			this->Column2->HeaderText = L"Humedad";
-			this->Column2->MinimumWidth = 6;
-			this->Column2->Name = L"Column2";
-			this->Column2->Width = 125;
+			this->Humedad->HeaderText = L"Humedad";
+			this->Humedad->MinimumWidth = 6;
+			this->Humedad->Name = L"Humedad";
+			this->Humedad->Width = 125;
 			// 
-			// Column3
+			// NivelCO
 			// 
-			this->Column3->HeaderText = L"Concentracion CO";
-			this->Column3->MinimumWidth = 6;
-			this->Column3->Name = L"Column3";
-			this->Column3->Width = 125;
+			this->NivelCO->HeaderText = L"NivelCO";
+			this->NivelCO->MinimumWidth = 6;
+			this->NivelCO->Name = L"NivelCO";
+			this->NivelCO->Width = 125;
 			// 
-			// Column4
+			// CalidadAire
 			// 
-			this->Column4->HeaderText = L"Calidad Aire";
-			this->Column4->MinimumWidth = 6;
-			this->Column4->Name = L"Column4";
-			this->Column4->Width = 125;
+			this->CalidadAire->HeaderText = L"CalidadAire";
+			this->CalidadAire->MinimumWidth = 6;
+			this->CalidadAire->Name = L"CalidadAire";
+			this->CalidadAire->Width = 125;
 			// 
 			// Column5
 			// 
@@ -376,9 +412,9 @@ namespace WeatherStationView {
 			// 
 			// SensorMaintenanceForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(761, 361);
+			this->ClientSize = System::Drawing::Size(1015, 444);
 			this->Controls->Add(this->UnidadTBox);
 			this->Controls->Add(this->UnidadTLabel);
 			this->Controls->Add(this->UbigeoCombo);
@@ -396,6 +432,7 @@ namespace WeatherStationView {
 			this->Controls->Add(this->COLabel);
 			this->Controls->Add(this->HumLabel);
 			this->Controls->Add(this->TempLabel);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"SensorMaintenanceForm";
 			this->Text = L"SensorMaintenanceForm";
 			this->Load += gcnew System::EventHandler(this, &SensorMaintenanceForm::SensorMaintenanceForm_Load);
@@ -410,7 +447,7 @@ namespace WeatherStationView {
 	}
 
 private: System::Void AgregarBtn_Click(System::Object^ sender, System::EventArgs^ e) {
-	double Temperatura = Int32::Parse(TempBox->Text);
+	int Temperatura = Int32::Parse(TempBox->Text);
 	int Humedad = Int32::Parse(HumBox->Text);
 	int ConcentracionCO = Int32::Parse(COBox->Text);
 	int AirQ = Int32::Parse(AirQBox->Text);
@@ -477,12 +514,18 @@ private: System::Void AgregarBtn_Click(System::Object^ sender, System::EventArgs
 			   Ambiente^ ambiente = sensorData[i];
 
 			   dataGridView1->Rows->Add(gcnew array<String^> {
-				   "" + ambiente->IdMedicion,
+					   "" + ambiente->IdMedicion,
 					   "" + dynamic_cast<SensorTemperaturaHumedad^>(ambiente->DataBase[0])->Temperatura, //temperatura
-					   dynamic_cast<SensorTemperaturaHumedad^>(ambiente->DataBase[0])->UnidadTemp,
+						   dynamic_cast<SensorTemperaturaHumedad^>(ambiente->DataBase[0])->UnidadTemp,
 					   "" + dynamic_cast<SensorTemperaturaHumedad^>(ambiente->DataBase[0])->Humedad, //humedad
 					   "" + dynamic_cast<SensorCO^>(ambiente->DataBase[1])->NivelCO, //co
 					   "" + dynamic_cast<SensorCalidadAire^>(ambiente->DataBase[2])->CalidadAire, //airq
+
+					   /*
+		
+					   "" + dynamic_cast<SensorCO^>(ambiente->DataBase[1])->NivelCO, //co
+					   "" + dynamic_cast<SensorCalidadAire^>(ambiente->DataBase[2])->CalidadAire, //airq
+					   */
 					   ambiente->UbicacionGeografica, //ubigeo
 					   ambiente->FechaMedicion, //hora
 					   ambiente->TiempoMedicion //hora
@@ -500,7 +543,7 @@ private: System::Void AgregarBtn_Click(System::Object^ sender, System::EventArgs
 
 private: System::Void ModificarBtn_Click(System::Object^ sender, System::EventArgs^ e) {
 
-	double Temperatura = Int32::Parse(TempBox->Text);
+	int Temperatura = Int32::Parse(TempBox->Text);
 	int Humedad = Int32::Parse(HumBox->Text);
 	int ConcentracionCO = Int32::Parse(COBox->Text);
 	int AirQ = Int32::Parse(AirQBox->Text);
